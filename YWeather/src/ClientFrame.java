18bb7bd15cc18fbd084a5fe4383c3eac129874c0
G0DZ <0apm.tv@gmail.com>
2015-10-17 02:37:39 +0300

    Финальное изменение перед стартом работы №2

--- YWeather/src/ClientFrame.java
+++ YWeather/src/ClientFrame.java
@@ -5,7 +5,6 @@ import java.awt.Point;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
